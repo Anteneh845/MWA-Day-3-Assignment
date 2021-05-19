1d0aa389038e921d4c486bb6913ce951b977dbbd
Anteneh <anteneh845@gmail.com>
2021-05-19 17:55:32 -0500

    Minor update on games controller

--- controllers/games.controller.js
+++ controllers/games.controller.js
@@ -4,10 +4,14 @@ module.exports.listGames = (req, res) => {
     let [offset, count] = [0, 3];
     let dbConnection = getDBConnection();
     let collection = dbConnection.collection("games");
-    if (req.query.offset && req.query.count) {
-        [offset, count] = [parseInt(req.query.offset), parseInt(req.query.count)];
+    if (req.query.offset)
+        offset = parseInt(req.query.offset);
+
+    if (req.query.count) {
+        count = parseInt(req.query.count)
         count = count > 7 ? 7 : count;
     }
+
     collection.find().skip(offset).limit(count).toArray((err, games) => {
         if (err) {
             console.log(err);
